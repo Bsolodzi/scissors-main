e2eba5d84e97ce1b4c423052d4015d46ea904439
Bernard Kwame Solodzi <86501778+Bsolodzi@users.noreply.github.com>
2023-08-19 19:48:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ limiter = Limiter(
     get_remote_address,
     app=app,
     default_limits=["200 per day", "50 per hour"]
-    storage_uri="memory://",
+    # storage_uri="memory://",
 )
 db = SQLAlchemy(app)
 login_manager = LoginManager()
